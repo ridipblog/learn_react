7d6040a30d8f89f973c3aa0799c3412f01f24d5d
unknown <memorytemp5@gmail.com>
2023-09-19 20:38:07 +0530

    19-09-23

--- frist-web/src/components/TextApp/TextApp.js
+++ frist-web/src/components/TextApp/TextApp.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import "./class.css";
 import "./TextApp.css";
 import DummyText from "./DummyText";
+var temp_word = "";
+var index = 0;
 export default function TextApp(props) {
   const [text, setText] = useState("");
   const [char, setChar] = useState(0);
@@ -10,30 +12,31 @@ export default function TextApp(props) {
   const [seconds, setSeconds] = useState(0);
   const [isStart, setIsStart] = useState(false);
   const dummy_text_arr = props.dummyTextProps.split(" ");
-  var index = 0;
-  var current = 0;
-  var wrong = 0;
+  const [currect, setCurrect] = useState(0);
+  const [wrong, setWrong] = useState(0);
+  const [disabled, setDisabled] = useState(true);
+  const [startDisabled, setStartDisabled] = useState(false);
   const changeText = (event) => {
     var letter = event.target.value;
-    if (props.dummyTextProps.length >= letter.length) {
-      var temp_word = "";
-      console.log(letter);
-      if (letter === " ") {
-        console.log(letter);
+    if (letter[letter.length - 1] === "\n") {
+      console.log("Enter");
+    }
+    if (letter.length <= props.dummyTextProps.length) {
+      if (letter[letter.length - 1] === " ") {
         if (temp_word === dummy_text_arr[index]) {
-          current = current + 1;
+          setCurrect((currect) => currect + 1);
+          console.log(currect);
+          console.log("Currect", currect);
         } else {
-          wrong = wrong + 1;
+          setWrong(wrong + 1);
+          console.log("Wrong", wrong);
         }
-        console.log(temp_word);
-        temp_word = "";
         index++;
+        temp_word = "";
       } else {
-        temp_word = temp_word + letter;
+        temp_word = temp_word + letter[letter.length - 1];
       }
     }
-    console.log(current);
-    console.log(wrong);
     setText(letter);
     setChar(letter.length);
     var allText = letter;
@@ -47,19 +50,32 @@ export default function TextApp(props) {
 
   const startTimer = () => {
     setIsStart(true);
+    setDisabled(false);
+    setStartDisabled(true);
   };
   const stopTimer = () => {
     setIsStart(false);
     setSeconds(0);
+    index = 0;
+    temp_word = "";
+    setStartDisabled(false);
+    setDisabled(true);
   };
+
   //    Timer Functions
   useEffect(() => {
     let timer;
+    const handleBackspace = (e) => {
+      if (e.key === "Backspace") {
+        e.preventDefault();
+      }
+    };
+    document.addEventListener("keydown", handleBackspace);
     if (isStart) {
       timer = setInterval(() => {
         setSeconds((prev) => prev + 1);
       }, 1000);
-      if (seconds > 5) {
+      if (seconds > 10) {
         stopTimer();
       }
     } else {
@@ -76,7 +92,7 @@ export default function TextApp(props) {
         <div className="flex_div result_div">
           <h1 className="heading">{props.TextAppProps.title}</h1>
           <p>Your Total Characters : {char}</p>
-          <p>Your Total Words : {word}</p>
+          <p>Your Total Words : {word / 1}</p>
           <p>Timer : {seconds}seconds</p>
         </div>
         <div className="flex_div tool_div">
@@ -84,16 +100,27 @@ export default function TextApp(props) {
             value={text}
             onChange={changeText}
             placeholder={props.TextAppProps.place}
+            disabled={disabled}
           ></textarea>
           {/* <button onClick={convertText}>Convert</button> */}
           <div className="flex_div performance_div">
             <div className="flex_div btn_div">
-              <button onClick={startTimer}>Start Timer</button>
+              <button onClick={startTimer} disabled={startDisabled}>
+                Start Timer
+              </button>
               <button onClick={stopTimer}>Stop Timer</button>
             </div>
             <div className="flex_div performance_div_1">
-              <p>Your Word Per Minute: 0</p>
-              <p>Your Accuracy: 0</p>
+              {isStart ? (
+                <p>No Result Yet</p>
+              ) : (
+                <>
+                  <p>Your Word Per Minute: {word}</p>
+                  <p>Your Accuracy: {((currect / word) * 100).toFixed(2)}</p>
+                  <p>Total Currect Words: {currect}</p>
+                  <p>Total Wrong Word: {wrong}</p>
+                </>
+              )}
             </div>
           </div>
         </div>
